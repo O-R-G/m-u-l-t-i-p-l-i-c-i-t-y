62520da1958a9b48024361df7b8a31b11dd0e981
wei <wei@o-r-g.com>
2025-10-30 23:14:22 +0800

    update screenfull and windowfull

--- static/php/config.php
+++ static/php/config.php
@@ -3,7 +3,14 @@
     site-specific config
 */
 
+$site_title = 'M-u-l-t-i-p-l-i-c-i-t-y';
 $head = 'HUM 598';
 $site = 'M-u-l-t-i-p-l-i-c-i-t-y';
 $home = $head . ", " . $site;
+$card_default = '/media/jpg/card-default.jpg';
+$logo_src = '/media/jpg/logo.jpg';
+$description = 'Hello, w-w-w-orld.';
+$site_url = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+$site_url .= '://' . $_SERVER['SERVER_NAME'];
+$og_locale = 'en_US';
 ?>
